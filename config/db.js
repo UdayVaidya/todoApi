2f6768ba36b908693cd74b632be6d85a936ef5ac
udayvaidya13 <udayvaidya13@gmail.com>
2024-11-15 17:22:03 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from a .env file
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/todoList');
-    console.log('MongoDB connected successfully');
+    const mongoURI = process.env.MONGO_URI ;
+    const dbName = process.env.DB_NAME ;
+
+    await mongoose.connect(`${mongoURI}/${dbName}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected successfully to database: ${dbName}`);
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
